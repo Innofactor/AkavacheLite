994156ed0dd83c9f4cd614f74f893745ec6920a8
Alexey Shytikov <alexey.shytikov@cinteros.se>
2018-12-13 15:16:45 +0100

    Correcting namespace

--- src/Akvache.Dynamics/Implementations/PersistentBlobCache.cs
+++ src/Akvache.Dynamics/Implementations/PersistentBlobCache.cs
@@ -1,4 +1,4 @@
-﻿namespace AkavacheLite.Dynamics.Implementations
+﻿namespace Akavache.Dynamics.Implementations
 {
     using System;
     using System.Collections.Generic;
@@ -6,7 +6,7 @@
     using System.Linq;
     using System.Threading;
     using System.Threading.Tasks;
-    using AkavacheLite.Dynamics.Interfaces;
+    using Akavache.Dynamics.Interfaces;
     using Akvache.Dynamics.Structure;
     using Newtonsoft.Json;
     using SQLite;
